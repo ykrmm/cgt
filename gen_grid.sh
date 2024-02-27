76577ed397eceda22ca35e9eef9f252670c0ca51
Yannis KARMIM <karmimy@gpuserver4-master.cnam.fr>
2024-02-27 23:39:12 +0100

    grid search

---
+++ gen_grid.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+CONFIG=CGT/cifar10-CGT
+GRID=CGT/grid_cgt_soft
+REPEAT=1
+MAX_JOBS=4
+
+# generate configs (after controlling computational budget)
+# please remove --config_budget, if don't control computational budget
+python configs_gen.py --config configs/${CONFIG}.yaml \
+  --grid configs/${GRID}.txt \
+  --out_dir configs
---
+++ run_grid.sh
@@ -0,0 +1,20 @@
+CONFIG_DIR=/users/k/karmimy/These/cgt/configs/cifar10-CGT_grid_grid_cgt_soft
+REPEAT=1
+MAX_JOBS=4
+MAIN=${4:-main}
+(
+  trap 'kill 0' SIGINT
+  CUR_JOBS=0
+  for CONFIG in "$CONFIG_DIR"/*.yaml; do
+    echo $CONFIG
+    if [ "$CONFIG" != "$CONFIG_DIR/*.yaml" ]; then
+      ((CUR_JOBS >= MAX_JOBS)) && wait -n
+      echo "Job launched: $CONFIG"
+      python $MAIN.py --cfg $CONFIG --repeat $REPEAT --mark_done &
+      ((CUR_JOBS < MAX_JOBS)) && sleep 1
+      ((++CUR_JOBS))
+    fi
+  done
+
+  wait
+)
